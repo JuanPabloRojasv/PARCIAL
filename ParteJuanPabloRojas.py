6f67dda2956e320ceba2a57f692b91b60ce71479
Juan <jprojas95395@umanizales.edu.co>
2022-05-06 17:19:09 -0500

    parcial2.1

---
+++ ParteJuanPabloRojas.py
@@ -0,0 +1,11 @@
+print(" Punto de parcial ")
+
+print(input("solicite lista de numeros: "))
+lista= ["1", "4", "3", "2",]
+cantidad = len(lista)
+
+print("La cantidad es: ")
+print(cantidad)
+print("Originalmente, la lista es: ", lista)
+lista.reverse()
+print("Y al invertirla internamente, es: ", lista)
